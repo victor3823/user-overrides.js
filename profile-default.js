291068a1e04eb4b99a3de1a186fc0270e6772023
dedbepole <37631552+dedbepole@users.noreply.github.com>
2021-03-04 14:18:34 +0000

    Restore session for default profile

--- profile-default.js
+++ profile-default.js
@@ -4,6 +4,9 @@ user_pref("_user.js.parrot", "Default profile: syntax error");
 // 2802: save history
 user_pref("privacy.sanitize.sanitizeOnShutdown", false);
 
+// 0102: restore session on startup
+user_pref("browser.startup.page", 3);
+
 // Enable Firefox Accounts
 user_pref("identity.fxaccounts.enabled", true);
 
