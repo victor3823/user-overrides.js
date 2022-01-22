a489c3d85991e25e60dedc28322a122fef9bc7a2
dedbepole <37631552+dedbepole@users.noreply.github.com>
2022-01-22 13:03:54 +0000

    Use built-in cookie clearing instead of CAD

--- profile-default.js
+++ profile-default.js
@@ -1,13 +1,12 @@
 user_pref("_user.js.parrot", "Default profile: syntax error");
 
 
-// 2802: save history
-user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+// 2811: Clear-on-shutdown items
+user_pref("privacy.clearOnShutdown.downloads", false); // [DEFAULT: true]
+user_pref("privacy.clearOnShutdown.history", false);   // [DEFAULT: true]
 
-/* 2801: save cookies
- * 0=keep until they expire (default), 2=keep until you close Firefox
- */
-user_pref("network.cookie.lifetimePolicy", 0);
+// 2812: reset default items to clear with Ctrl-Shift-Del (to match 2811)
+user_pref("privacy.cpd.history", false);  // [DEFAULT: true]
 
 // 0102: restore session on startup
 user_pref("browser.startup.page", 3);
