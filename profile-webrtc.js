d27a8d22cbe12baa8cee32e38a0b697a1bb7d375
dedbepole <37631552+dedbepole@users.noreply.github.com>
2022-01-22 12:21:13 +0000

    Bring the WebRTC profile up to date

--- profile-webrtc.js
+++ profile-webrtc.js
@@ -1,38 +1,18 @@
 user_pref("_user.js.parrot", "WebRTC profile: syntax error");
 
 
-// 2802: Save history
-user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+// 2811: Clear-on-shutdown items
+user_pref("privacy.clearOnShutdown.downloads", false); // [DEFAULT: true]
+user_pref("privacy.clearOnShutdown.history", false);   // [DEFAULT: true]
 
-/* 2801: save cookies
- * 0=keep until they expire (default), 2=keep until you close Firefox
- */
-user_pref("network.cookie.lifetimePolicy", 0);
+// 2812: reset default items to clear with Ctrl-Shift-Del (to match 2811)
+user_pref("privacy.cpd.history", false);  // [DEFAULT: true]
 
 // 4501: Disable RFP
 user_pref("privacy.resistFingerprinting", false);
 
-// 2001: Enable WebRTC (Web Real-Time Communication)
-user_pref("media.peerconnection.enabled", true);
-
-// 2002: limit WebRTC IP leaks if using WebRTC
-user_pref("media.peerconnection.ice.default_address_only", false);
-user_pref("media.peerconnection.ice.no_host", false); // [FF51+]
-user_pref("media.peerconnection.ice.proxy_only_if_behind_proxy", false); // [FF70+]
-
-// 2022: Enable screensharing
-user_pref("media.getusermedia.screensharing.enabled", true);
-user_pref("media.getusermedia.browser.enabled", true);
-user_pref("media.getusermedia.audiocapture.enabled", true);
-
-// 2510: Enable Web Audio API [FF51+]
-user_pref("dom.webaudio.enabled", true);
-
 // 2010: Enable WebGL (Web Graphics Library)
 user_pref("webgl.disabled", false);
-
-// 2012: unlimit WebGL
-user_pref("webgl.min_capability_mode", false);
 
 
 user_pref("_user.js.parrot", "SUCCESS: No no he's not dead, he's, he's restin'!");
