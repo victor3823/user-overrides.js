65f00fcf256e2c24fb09927f5bcc35f2be9c712d
victor3823 <37631552+dedbepole@users.noreply.github.com>
2024-05-07 22:41:21 +0100

    Use override recipe for WebRTC profile

--- profile-webrtc.js
+++ profile-webrtc.js
@@ -1,18 +1,9 @@
 user_pref("_user.js.parrot", "WebRTC profile: syntax error");
 
 
-// 2811: Clear-on-shutdown items
-user_pref("privacy.clearOnShutdown.downloads", false); // [DEFAULT: true]
-user_pref("privacy.clearOnShutdown.history", false);   // [DEFAULT: true]
-
-// 2812: reset default items to clear with Ctrl-Shift-Del (to match 2811)
-user_pref("privacy.cpd.history", false);  // [DEFAULT: true]
-
-// 4501: Disable RFP
-user_pref("privacy.resistFingerprinting", false);
-
-// 2010: Enable WebGL (Web Graphics Library)
-user_pref("webgl.disabled", false);
+/* override recipe: RFP is not for me ***/
+user_pref("privacy.resistFingerprinting", false); // 4501
+user_pref("webgl.disabled", false); // 4520 [mostly pointless if not using RFP]
 
 
 user_pref("_user.js.parrot", "SUCCESS: No no he's not dead, he's, he's restin'!");
