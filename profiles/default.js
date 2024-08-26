cd5a0b9056c35de7b806e5e4c9449320c9727961
victor3823 <37631552+dedbepole@users.noreply.github.com>
2024-08-26 22:43:46 +0100

    Update session restore recipe

--- profiles/default.js
+++ profiles/default.js
@@ -3,14 +3,14 @@ user_pref("_user.js.parrot", "Default profile: syntax error");
 
 /* override recipe: enable session restore ***/
 user_pref("browser.startup.page", 3); // 0102
-user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
-  // user_pref("browser.sessionstore.privacy_level", 0); // 1003 optional to restore cookies/formdata
-user_pref("privacy.clearOnShutdown.history", false); // 2811
-user_pref("privacy.clearOnShutdown.downloads", false); // [DEFAULT: true]
-user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // [FF128+] [DEFAULT: true]
-user_pref("privacy.cpd.history", false); // 2820 optional to match when you use Ctrl-Shift-Del
-user_pref("privacy.clearHistory.historyFormDataAndDownloads", false);
-user_pref("privacy.clearSiteData.historyFormDataAndDownloads", false);
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2811 FF128+
+
+// 2820 optional to match when you use settings>Cookies and Site Data>Clear Data
+user_pref("privacy.clearSiteData.historyFormDataAndDownloads", false); // FF128+
+
+// 2830 optional to match when you use Ctrl-Shift-Del (settings>History>Custom Settings>Clear History)
+user_pref("privacy.clearHistory.historyFormDataAndDownloads", false); // FF128+
+
 
 // Enable Firefox Accounts
 user_pref("identity.fxaccounts.enabled", true);
