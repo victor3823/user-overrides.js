385a8e9afa3296aa9c7a3e4d2ad67c6c48798714
victor3823 <37631552+dedbepole@users.noreply.github.com>
2025-03-07 22:40:08 +0000

    Migrate sanitizing prefs to arkenfox v135 (FF136)

--- profiles/default.js
+++ profiles/default.js
@@ -4,12 +4,15 @@ user_pref("_user.js.parrot", "Default profile: syntax error");
 /* override recipe: enable session restore ***/
 user_pref("browser.startup.page", 3); // 0102
 user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2811 FF128+
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false); // 2812 FF136+
 
 // 2820 optional to match when you use settings>Cookies and Site Data>Clear Data
 user_pref("privacy.clearSiteData.historyFormDataAndDownloads", false); // FF128+
+user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", false); // 2821 FF136+
 
 // 2830 optional to match when you use Ctrl-Shift-Del (settings>History>Custom Settings>Clear History)
 user_pref("privacy.clearHistory.historyFormDataAndDownloads", false); // FF128+
+user_pref("privacy.clearHistory.browsingHistoryAndDownloads", false); // 2831 FF136+
 
 
 /* RFPTargets (FPP overrides) */
